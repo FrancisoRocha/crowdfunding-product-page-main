1b8c9778056a5792d6644f0f0b21f35ae6247ee2
Francisco Rocha <122055396+FrancisoRocha@users.noreply.github.com>
2024-08-03 23:20:12 -0600

    modal agregado

---
+++ modal.js
@@ -0,0 +1,19 @@
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const openBoton = document.querySelector('.btn-open');
+const closeBoton = document.querySelector('.btn-close');
+
+
+const btnOpen = function (){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = function (){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+openBoton.addEventListener('click', btnOpen)
+closeBoton.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
